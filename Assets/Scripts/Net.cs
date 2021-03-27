3ea331b71327a42dc96d05907a0fc89efecc0b94
TheMemeMeister <60407375+TheMemeMeister@users.noreply.github.com>
2021-03-27 03:20:53 -0400

    4th Bumper, Lives counter
    Lives counter added for later (when I get through reqs and into polish) . TODO scenes -> start scene, end scene.

--- Assets/Scripts/Net.cs
+++ Assets/Scripts/Net.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Net : MonoBehaviour
 {
     public Rigidbody r_ball;
-    
     public GameObject ballspawn;
-    // Start is called before the first frame update
+    int lives = 3; //can use this later to implement a lives system and end screen
+    
+
     void Start()
     {
 
@@ -17,16 +18,20 @@ public class Net : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (lives == 0 )
+        {
+            //obs this is where I switch scenes to lose scene 
+        }
     }
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
+            //resets position of the ball when ball falls out of the play area 
             r_ball.velocity = Vector3.zero;
             r_ball.transform.position = ballspawn.transform.position;
             Debug.Log("Ball fell out of bounds");
-
+            lives--; 
 
         }
     }
